492747b0529329b4b22b8b79d8d7f360edb47f22
ruuinnxx <tokzhigitova.gb@gmail.com>
2025-10-17 20:49:53 +0500

    6 HomeWork

---
+++ HomeWork6/Proj1_Strategy/Program.cs
@@ -0,0 +1,75 @@
+﻿﻿var context = new PaymentContext();
+
+while (true)
+{
+    Console.WriteLine("Выберите способ оплаты" +
+                      "\n1 - PayPal" +
+                      "\n2 - CreditCard" +
+                      "\n3 - Crypto");
+    var strategy = Console.ReadLine();
+    switch (strategy)
+    {
+        case "1":
+            context.SetStrategy(new PayPal());
+            break;
+        case "2":
+            context.SetStrategy(new CreditCard());
+            break;
+        case "3":
+            context.SetStrategy(new Crypto());
+            break;
+        default: 
+            throw new Exception("Нет такого выбора");
+    }
+
+    context.PayStrategy(5000);
+    Console.WriteLine();
+}
+
+public interface IPaymentStrategy
+{
+    void Pay(decimal sum);
+}
+
+public class PayPal : IPaymentStrategy
+{
+    public void Pay(decimal sum)
+    {
+        Console.WriteLine("Оплата: PayPal");
+    }
+}
+
+public class CreditCard : IPaymentStrategy
+{
+    public void Pay(decimal sum)
+    {
+        Console.WriteLine("Оплата: Кредитная карта");
+    }
+}
+
+public class Crypto : IPaymentStrategy
+{
+    public void Pay(decimal sum)
+    {
+        Console.WriteLine("Оплата: Криптовалюта");
+    }
+}
+
+public class PaymentContext
+{
+    private IPaymentStrategy? _paymentStrategy;
+    
+    public void SetStrategy(IPaymentStrategy strategy)
+    {
+        _paymentStrategy = strategy;
+    }
+
+    public void PayStrategy(int sum)
+    {
+        if (_paymentStrategy == null) 
+            return;
+        Console.WriteLine($"Выбрана: {_paymentStrategy} стратегия");
+        _paymentStrategy.Pay(sum);
+        Console.WriteLine("Оплата прошла успешна");
+    }
+}
---
+++ HomeWork6/Proj2_Observer/Program.cs
@@ -0,0 +1,129 @@
+﻿﻿using Project2_Observer;
+
+var exchange = new CurrencyExchange();
+
+var dollar = new DollarObserver();
+var euro = new EuroObserver();
+var ruble = new RubleObserver();
+
+exchange.AddSubscriber(dollar);
+exchange.AddSubscriber(euro);
+exchange.AddSubscriber(ruble);
+
+exchange.EurToKzt = 485.2;
+exchange.UsdToKzt = 512.7;
+exchange.RubToKzt = 5.02;
+
+Console.WriteLine("Евро отписалась");
+
+exchange.RemoveSubscriber(euro);
+
+Console.WriteLine("Новые цены");
+exchange.UsdToKzt = 486.5;
+exchange.EurToKzt = 511.3;
+exchange.RubToKzt = 5.10;
+
+
+namespace Project2_Observer
+{
+    public interface IObserver
+    {
+        void Update(ISubject subject);
+    }   
+
+    public interface ISubject
+    {
+        void AddSubscriber(IObserver observer);
+        void RemoveSubscriber(IObserver observer);
+        void NotifySubscriber();
+    
+    }
+
+    public class CurrencyExchange : ISubject
+    {
+        private readonly List<IObserver> _observers = new ();
+        private double _usdToKzt;
+        private double _rubToKzt;
+        private double _eurToKzt;
+
+        public double UsdToKzt
+        {
+            get => _usdToKzt;
+            set
+            {
+                _usdToKzt = value;
+                NotifySubscriber();
+            }
+        }
+    
+        public double RubToKzt
+        {
+            get => _rubToKzt;
+            set
+            {
+                _rubToKzt = value;
+                NotifySubscriber();
+            }
+        }
+    
+        public double EurToKzt
+        {
+            get => _eurToKzt;
+            set
+            {
+                _eurToKzt = value;
+                NotifySubscriber();
+            }
+        }
+    
+        public void AddSubscriber(IObserver observer)
+        {
+            Console.WriteLine($"Субъект: {observer} подписался на издателя");
+            _observers.Add(observer);
+        }
+
+        public void RemoveSubscriber(IObserver observer)
+        {
+            Console.WriteLine($"Субъект: {observer} отписался от издателя");
+            _observers.Remove(observer);
+        }
+
+        public void NotifySubscriber()
+        {
+            Console.WriteLine("Субъект: Уведомление подписчиков");
+            foreach (var observer in _observers)
+            {
+                observer.Update(this);
+            }
+        }
+    }
+
+    public class DollarObserver : IObserver
+    {
+
+        public void Update(ISubject subject)
+        {
+            if (subject is CurrencyExchange exchange)
+                Console.WriteLine($"Курс USD обновлен: {exchange.UsdToKzt} KZT");
+        }
+    }
+
+    public class RubleObserver : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if (subject is CurrencyExchange exchange)
+                Console.WriteLine($"Курс RUBLE обновлен: {exchange.RubToKzt} KZT");
+        }
+    }
+
+    public class EuroObserver : IObserver
+    {
+
+        public void Update(ISubject subject)
+        {
+            if (subject is CurrencyExchange exchange)
+                Console.WriteLine($"Курс EURO обновлен: {exchange.EurToKzt} KZT");
+        }
+    }
+}
